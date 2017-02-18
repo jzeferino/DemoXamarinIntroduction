9b4e4caf8ea0f6ee304decd4316af8abedd54606
jzeferino <jorgevalentzeferino@gmail.com>
2017-02-18 21:04:09 +0000

    Updated UWP packages.

--- 2-DemoXamarinIntroductionForms/Universal/App.xaml.cs
+++ 2-DemoXamarinIntroductionForms/Universal/App.xaml.cs
@@ -18,9 +18,6 @@ namespace DemoXamarinIntroductionForms.Universal
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }

7cc5e9d7b1362e0250ec9056e78f3759bcbdf3d7
jzeferino <jorgevalentzeferino@gmail.com>
2018-02-22 12:38:34 +0000

    Updated xamarin forms.

--- 1-DemoXamarinIntroductionNative/iOS/ViewController.designer.cs
+++ 1-DemoXamarinIntroductionNative/iOS/ViewController.designer.cs
@@ -1,40 +1,39 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
-
-using System.CodeDom.Compiler;
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace DemoXamarinIntroductionNative.iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
         [Outlet]
         UIButton Button { get; set; }
 
         [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UIImageView ImagePhoto { get; set; }
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView ImagePhoto { get; set; }
 
         [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UIButton PhotoButton { get; set; }
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton PhotoButton { get; set; }
 
-        void ReleaseDesignerOutlets()
+        void ReleaseDesignerOutlets ()
         {
-            if (ImagePhoto != null)
-            {
-                ImagePhoto.Dispose();
+            if (ImagePhoto != null) {
+                ImagePhoto.Dispose ();
                 ImagePhoto = null;
             }
-            if (PhotoButton != null)
-            {
-                PhotoButton.Dispose();
+
+            if (PhotoButton != null) {
+                PhotoButton.Dispose ();
                 PhotoButton = null;
             }
         }
